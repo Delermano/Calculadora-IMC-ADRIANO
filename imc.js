72aedad6df2c056f170ebacf0c40846b01ccbec6
adriano <adriano.delermano@gmail.com>
2025-05-01 18:23:14 -0300

    atual

--- imc.js
+++ imc.js
@@ -21,7 +21,7 @@ function imc() {
         }else if (valorIMC < 30){
             classificacao = 'levemente acima do peso.';
         }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau .';
+            classificacao = 'com obesidade grau 1.';
         }else if (valorIMC < 40){
             classificacao = 'com obesidade grau 2.';
         }else{
